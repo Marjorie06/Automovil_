20cfc8c53d562df0649885c4d81e32a4e90aa96e
kar-gl <karenanaluisa40@gmail.com>
2024-12-01 21:56:55 -0500

    cambio

---
+++ AutomovilProyecto/src/main/java/com/mycompany/automovilproyecto/AutomovilProyecto.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.automovilproyecto;
+
+/**
+ *
+ * @author Usuario
+ */
+public class AutomovilProyecto {
+
+    public static void main(String[] args) {
+    }
+}
